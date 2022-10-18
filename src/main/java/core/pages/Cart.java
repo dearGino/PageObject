29ba4638fe8b49dd38cd0501864e834f0143c483
Anna Persh <dearest.gino@gmail.com>
2022-10-19 01:46:24 +0500

    fixed the function that returns the index

--- src/main/java/core/pages/Cart.java
+++ src/main/java/core/pages/Cart.java
@@ -18,18 +18,19 @@ public class Cart {
     }
     
     
-    private int getProductIfExist (String nameValue) {
+    private int getIndexIfProductExists (String nameValue) {
+    	int index = -1;
     	try{
     		for(Product product : products) {
     			if (product.getName().toUpperCase().contains(nameValue.toUpperCase())){
-    				return products.indexOf(product);
+    				index = products.indexOf(product);
     				}
     			}
     	}
     	catch (Exception exc){
     		System.out.println("Такого продукта нет в корзине");
     	}
-    	return -1;
+    	return index;
     }
     
     
@@ -37,7 +38,7 @@ public class Cart {
     public void addProductToCart(String name, String price, int count, boolean warranty12, boolean warranty24){
 		Product product = new Product();
 		product.setName(name);
-		product.setPrice(price);
+		product.setPrice(price.replaceAll("[^0-9]",""));
 		product.setCount(count);
 		product.setWarranty12(warranty12);
 		product.setWarranty24(warranty24);
@@ -51,47 +52,46 @@ public class Cart {
     
     //Убрать товар из корзины полностью ????всё или количество уменьшить?
     public void removeProductFromCartByName(String nameValue){
-    	int x = getProductIfExist(nameValue);
-    	products.remove(products.indexOf(x));
+    	int indexOfProduct = getIndexIfProductExists(nameValue);
+    	products.remove(indexOfProduct);
     }
     
     
     public void increaseCountOfProductByName(String nameValue){
-    	int x = getProductIfExist(nameValue);
-    	int currentCount = products.get(x).getCount();
-    	products.get(x).setCount(currentCount+1);
+    	int indexOfProduct = getIndexIfProductExists(nameValue);
+    	int currentCount = products.get(indexOfProduct).getCount();
+    	products.get(indexOfProduct).setCount(currentCount+1);
     }
    
     
     public void decreaseCountOfProductByName(String nameValue){
-    	int x = getProductIfExist(nameValue);
-    	int currentCount = products.get(x).getCount();
+    	int indexOfProduct = getIndexIfProductExists(nameValue);
+    	int currentCount = products.get(indexOfProduct).getCount();
     	if(currentCount == 1) {
     		removeProductFromCartByName(nameValue);
     	}
     	else {
-    		products.get(x).setCount(currentCount-1);
+    		products.get(indexOfProduct).setCount(currentCount-1);
     	}
-    }
-            	
+    }  	
     
     public String getProductPrice (String nameValue){
-    	int x = getProductIfExist(nameValue);
-    	return products.get(x).getPrice();
+    	int indexOfProduct = getIndexIfProductExists(nameValue);
+    	return products.get(indexOfProduct).getPrice();
     }
     
     public String getProductName (String nameValue){
-    	int x = getProductIfExist(nameValue);
-    	return products.get(x).getName();
+    	int indexOfProduct = getIndexIfProductExists(nameValue);
+    	return products.get(indexOfProduct).getName();
     }
     
     public boolean getProductWarranty12 (String nameValue){
-    	int x = getProductIfExist(nameValue);
-    	return products.get(x).getWarranty12();
+    	int indexOfProduct = getIndexIfProductExists(nameValue);
+    	return products.get(indexOfProduct).getWarranty12();
     }
     
     public boolean getProductWarranty24 (String nameValue){
-    	int x = getProductIfExist(nameValue);
-    	return products.get(x).getWarranty24();
+    	int indexOfProduct = getIndexIfProductExists(nameValue);
+    	return products.get(indexOfProduct).getWarranty24();
     }
 }
