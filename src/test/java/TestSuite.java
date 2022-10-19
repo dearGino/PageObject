7c38a222c3b147fd980601c99635c7e1ebb445e4
Anna Persh <dearest.gino@gmail.com>
2022-10-19 23:32:15 +0500

    Delete TestSuite.java

--- src/test/java/TestSuite.java
+++
@@ -1,9 +0,0 @@
-package test.java;
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-import org.junit.runners.Suite.SuiteClasses;
-
-@RunWith(Suite.class)
-@SuiteClasses({ BaseTest.class })
-public class TestSuite {
-}
